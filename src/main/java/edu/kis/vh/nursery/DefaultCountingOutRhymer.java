063b7aca2dc541b7e17c610a690b24254cddd46d
ADAsss98 <224806@edu.p.lodz.pl>
2021-02-19 19:27:39 +0100

    dodanie stałej do INITIAL

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,32 +4,32 @@ public class DefaultCountingOutRhymer {
 
 	private static final int TAB_SIZE = 12;
 	private static final int LAST_TABLE_INDEX = TAB_SIZE -1;
-	private int initial = -1;
+	private static int INITIAL = -1;
 	private static final int RETURN_VALUE = -1;
 	private final int[] numbers = new int[TAB_SIZE];
 
 	public int getInitial() {
-		return initial;
+		return INITIAL;
 	}
 
 	protected void countIn(int in) {
 		if (!isFull())
-			numbers[++initial] = in;
+			numbers[++INITIAL] = in;
 	}
 
 	protected boolean callCheck() {
-			return initial == RETURN_VALUE;
+			return INITIAL == RETURN_VALUE;
 		}
 
 	protected boolean isFull() {
-			return initial == LAST_TABLE_INDEX;
+			return INITIAL == LAST_TABLE_INDEX;
 		}
 
 	protected int peekaboo() {
 		if (callCheck()) {
 			return RETURN_VALUE;
 		} else {
-			return numbers[initial];
+			return numbers[INITIAL];
 		}
 	}
 
@@ -37,7 +37,7 @@ public class DefaultCountingOutRhymer {
 		if (callCheck()) {
 			return RETURN_VALUE;
 		} else {
-			return numbers[initial--];
+			return numbers[INITIAL--];
 		}
 	}
 
